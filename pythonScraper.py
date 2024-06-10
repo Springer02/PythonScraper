a2e421b1e4b6fdb466a5c33d9b6771417a5bfdf1
Damien <105324747+Springer02@users.noreply.github.com>
2024-06-09 18:48:37 -0700

    Can now search and return a list of videos for someone to download from

--- pythonScraper.py
+++ pythonScraper.py
@@ -1,1 +1,25 @@
-print('Hello World')
+#Author: Damien Springer
+#Email: damienspringer02@gmail.com
+#Description: a music downloader that downloads highest quality rip it can find & added correct meta data 
+
+from pytube import YouTube
+from pytube import Search
+import logging ##To surpress warnings causes by youtube shorts 
+pytube_logger = logging.getLogger('pytube')
+pytube_logger.setLevel(logging.ERROR)
+
+#Manually choose a song to download 
+song = input("Enter song name")
+
+result = Search(song)
+i = 0
+for x in result.results:
+  print(str(i) + ": " + x.title)
+  i += 1
+  if (i == 5):
+    break
+
+i = int(input("Select song choice from list: "))
+yt = result.results[i]
+stream = yt.streams.get_audio_only()
+stream.download()
