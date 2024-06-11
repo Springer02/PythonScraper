64ac0dd9fe3254ab2ae2d9593a2b5afb6c7076d1
Damien <105324747+Springer02@users.noreply.github.com>
2024-06-11 15:54:05 -0700

    added functions, added spotipy

--- pythonScraper.py
+++ pythonScraper.py
@@ -5,21 +5,60 @@
 from pytube import YouTube
 from pytube import Search
 import logging ##To surpress warnings causes by youtube shorts 
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
 pytube_logger = logging.getLogger('pytube')
 pytube_logger.setLevel(logging.ERROR)
 
+auth_manager = SpotifyClientCredentials()
+sp = spotipy.Spotify(auth_manager=auth_manager)
+
+def main():
+  while True:
+    print("Options:")
+    print("1: Search for Single Song")
+    print("quit: Exit the Program")
+    userinput = input("User Choice: ")
+    if (userinput == "1"):
+      manuallyYoutubeSongSearch()
+    if (userinput == "quit"):
+      break
+
+ 
 #Manually choose a song to download 
-song = input("Enter song name")
+def manuallyYoutubeSongSearch():
+  while True:
+    song = input("Enter song name artist name")
+    result = Search(song)
+    i = 0
+    for x in result.results:
+      print(str(i) + ": " + x.title)
+      i += 1
+      if (i == 5):
+        break
 
-result = Search(song)
-i = 0
-for x in result.results:
-  print(str(i) + ": " + x.title)
-  i += 1
-  if (i == 5):
-    break
+    i = input("Select song choice from list (or back to search a new song): ")
+    if (i != "back"):
+      yt = result.results[int(i)]
+      stream = yt.streams.get_audio_only()
+      stream.download()
+      break
 
-i = int(input("Select song choice from list: "))
-yt = result.results[i]
-stream = yt.streams.get_audio_only()
-stream.download()
+def searchSong():
+  print("Options:")
+
+def searchArtist():
+  print("Options:")
+
+
+def searchAlbumArtist():
+  print("Options:")
+
+  
+#download song with metadata 
+def downloadSong():
+  print("Options:")
+
+if __name__ == "__main__":
+  main()
